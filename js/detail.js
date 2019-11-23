a66d5b82fe9bbe586b09e59e9d5dfda74d066379
锅 <596278576@qq.com>
2019-11-23 11:11:43 +0800

    实现数据存储本地存储中

--- js/detail.js
+++ js/detail.js
@@ -1,19 +1,25 @@
 $(()=>{
+    // 获取id
     let id=location.search.substring(1);
+    // 判断对应的id
     let shuju=phoneData.find(e=>{
         return e.pID=id;
     })
+    // 修改对应id的数据
             $('.sku-name').text(shuju.name);
             $('.summary-price em').text(shuju.price);
             $('.preview-img img').attr('src',(shuju.imgSrc));
 
 
+    
     $('.addshopcar').on('click',function(){
+        // 判断用户输入的值
         let num=$('.choose-number').val();
         if(parseInt(num)==0||isNaN(num)||num.trim().length==0){
             alert('不是数字');
             return;
         };
+        // 从本地存储获取数据
         let arr=Kits.getDate('cartData');
         console.log(arr);
         
@@ -21,10 +27,12 @@ $(()=>{
         let pd=arr.find(e=>{
             return e.pID=id;
         });
+        // 如果存在数量增加
         num=parseInt(num);
         if(pd){
             pd.num+=num;
         }else{
+            // 如果不存在则存储到对象中并把对象存储到数组中
             let obj={
                 pID:shuju.pID,
                 name:shuju.name,
@@ -34,7 +42,9 @@ $(()=>{
             }
             arr.push(obj);
         }
+        // 存入本地存储
         Kits.setDate('cartData',arr);
+        // 跳转
         location.href='./cart.html';
     })
 })
